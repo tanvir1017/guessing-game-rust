af7bdd9a12cbe22a5c6ca37fed9e241bc94a19dc
tanvir1017 <developer.tanvirhossain@gmail.com>
2024-11-05 01:14:47 +0600

    feat: 🎸 Finally have to learn a lot of things by this game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+// #![allow(unused)]   // -> will let you to have unused variable in your code
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io; // random -> rang
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut guess = String::new(); // store user given data in this mutable variable
+        println!("Guess the number!");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"), // match's arm
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You winn!");
+                break;
+            }
+        }
+    }
 }
